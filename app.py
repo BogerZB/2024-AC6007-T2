074d6d7868f9688e01c472cd82a722e284031d0f
Boger <bzhang029@e.ntu.edu.sg>
2025-02-16 11:00:45 +0000

    1

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
 from flask import Flask
-from flask import render_template, request
+from flask import render_template,request
 import textblob
 
 app = Flask(__name__)
 
-@app.route("/", methods = ["GET", "POST"])
+@app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
 
-@app.route("/SA", methods = ["GET", "POST"])
+@app.route("/SA",methods=["GET","POST"])
 def SA():
     return(render_template("SA.html"))
 
-@app.route("/SA_result", methods = ["GET", "POST"])
+@app.route("/SA_result",methods=["GET","POST"])
 def SA_result():
     q = request.form.get("q")
     r = textblob.TextBlob(q).sentiment
     return(render_template("SA_result.html", r=r))
+
+@app.route("/paynow",methods=["GET","POST"])
+def paynow():
+    return(render_template("paynow.html"))
 
 if __name__ == "__main__":
     app.run()
